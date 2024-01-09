5f5ffcd274df4146d5e77e91ba85a16a705300b8
IIDX19 RCHE <46210365+shiftdownet@users.noreply.github.com>
2024-01-10 00:05:21 +0900

    Implemented by decorator

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,14 +1,15 @@
 from django.urls import path
+from django.contrib.auth.decorators import login_required
 
 from . import views
 
 app_name = "accounts"
 urlpatterns = [
-    path("", views.ProfileView.as_view(), name="index"),
+    path("", login_required(views.ProfileView.as_view()), name="index"),
     path("login/", views.LoginView.as_view(), name="login"),
     path("logout/", views.LogoutView.as_view(), name="logout"),
-    path("profile/", views.ProfileView.as_view(), name="profile"),
-    path("password_change/", views.PasswordChange.as_view(), name="password_change"),
-    path("password_change/done/", views.PasswordChangeDone.as_view(), name="password_change_done"),
+    path("profile/", login_required(views.ProfileView.as_view()), name="profile"),
+    path("password_change/", login_required(views.PasswordChange.as_view()), name="password_change"),
+    path("password_change/done/", login_required(views.PasswordChangeDone.as_view()), name="password_change_done"),
 ]
 
--- overwork/views.py
+++ overwork/views.py
@@ -9,10 +9,9 @@ from overwork.models import OverworkApplication
 class TodaysOverworkApplicationsView(TemplateView ):
     template_name = "overwork/list.html"
 
-
-
     def get_context_data(self, **kwargs ):
         context = super().get_context_data(**kwargs)
+
         date = timezone.now()
         context["users"] = CustomUser.objects.prefetch_related(Prefetch("submitted_by", queryset=OverworkApplication.objects.filter(app_date=date), to_attr='app')).all()
 
